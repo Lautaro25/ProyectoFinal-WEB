eae7a44eff151b24044dec65c38e923e83a3c3b6
Lautaro <lautarovillabona.ar@gmail.com>
2024-10-07 20:17:04 -0300

    Anotaciones hechas

--- src/main/java/logica/cancha.java
+++ src/main/java/logica/cancha.java
@@ -1,11 +1,13 @@
 package logica;
 
 import java.io.Serializable;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class cancha implements Serializable {
@@ -15,15 +17,28 @@ public class cancha implements Serializable {
     @Basic
     private int numCancha;
     private boolean disponibilidad;
+    
+    @OneToMany(mappedBy = "cancha")
+    private List<reserva> reservas;
 
     public cancha() {
     }
 
-    public cancha(int id, int numCancha, boolean disponibilidad) {
+    public cancha(int id, int numCancha, boolean disponibilidad, List<reserva> reservas) {
         this.id = id;
         this.numCancha = numCancha;
         this.disponibilidad = disponibilidad;
+        this.reservas = reservas;
     }
+
+    public List<reserva> getReservas() {
+        return reservas;
+    }
+
+    public void setReservas(List<reserva> reservas) {
+        this.reservas = reservas;
+    }
+
 
     public int getId() {
         return id;
