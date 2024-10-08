806dc317df88f6c07f422cbfaf563dd92b096004
Lautaro <lautarovillabona.ar@gmail.com>
2024-10-08 11:29:27 -0300

    Anotaciones hechas

--- src/main/java/logica/reserva.java
+++ src/main/java/logica/reserva.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -21,45 +21,19 @@ public class reserva implements Serializable {
     private int idUsuarios;
     @Temporal(TemporalType.DATE)
     private Date fecha;
-    
-    @ManyToOne
-    @JoinColumn(name = "usuario_id")
-    private usuario usuario;
 
-    @ManyToOne
-    @JoinColumn(name = "cancha_id")
-    private cancha cancha;
-
-    @ManyToOne
-    @JoinColumn(name = "horario_id")
+    @OneToOne
+    @JoinColumn(name = "horario_id", referencedColumnName = "id")
     private horario horario;
 
     public reserva() {
     }
 
-    public reserva(int id, int idUsuarios, Date fecha, usuario usuario, cancha cancha, horario horario) {
+    public reserva(int id, int idUsuarios, Date fecha, horario horario) {
         this.id = id;
         this.idUsuarios = idUsuarios;
         this.fecha = fecha;
-        this.usuario = usuario;
-        this.cancha = cancha;
         this.horario = horario;
-    }
-
-    public usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public cancha getCancha() {
-        return cancha;
-    }
-
-    public void setCancha(cancha cancha) {
-        this.cancha = cancha;
     }
 
     public horario getHorario() {
@@ -69,8 +43,6 @@ public class reserva implements Serializable {
     public void setHorario(horario horario) {
         this.horario = horario;
     }
-
-
 
     public int getId() {
         return id;
